2da62477249ede6d0acdc657fc62a28a1d0966a3
Joeking360 <rdkilby@Dympna-Kilbys-iMac-4.local>
2016-11-08 21:02:57 +0000

    A list that you can scroll through
    A list that you can scroll through.

---
+++ List searcher.py
@@ -0,0 +1,56 @@
+#GitHub copy
+class Items:
+    name = ""
+    description = ""
+
+def clear():
+    print("\n"*50)
+
+
+counter = 0
+
+Health_potion = Items()
+Health_potion.name = "Health potion"
+Health_potion.description = "It heals you by 3 health points."
+
+Better_health_potion = Items()
+Better_health_potion.name = "Better ealth potion"
+Better_health_potion.description = "It heals you by 5 health points."
+
+Defence_potion = Items()
+Defence_potion.name = "Health potion"
+Defence_potion.description = "It raises your defence by 3 points."
+
+
+list1 = [Health_potion,Better_health_potion,Defence_potion]
+b = 1
+
+print("Use 'w' and 's' to search and use 'x' to select.")
+
+while b != "x":
+    b = input()
+    
+    clear()
+    if b == "w":
+        counter = counter - 1
+    elif b == "s":
+        counter = counter + 1
+    elif b == "x":
+        break
+        
+    else:
+        c = 0
+
+    for i in range(0,len(list1)):
+        print(list1[i].name)
+        
+        if i == counter:
+            a = list1[i].description
+            print(a)
+        
+        else:
+            c = 0
+        print()
+        
+print("You selected: " + list1[counter].name)
+
